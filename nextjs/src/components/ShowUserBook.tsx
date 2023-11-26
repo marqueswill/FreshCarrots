6a11643ce4a7f15cd4053ba01dd7f2bebe2ea1be
João Victor Pereira <institutodnb@gmail.com>
2023-11-26 13:23:48 -0300

    login page

--- nextjs/src/components/ShowUserBook.tsx
+++ nextjs/src/components/ShowUserBook.tsx
@@ -1,8 +1,7 @@
-import { UserBook } from "@/types/types";
 import styles from "@/styles/ShowUserBook.module.css";
 import { getAvailability } from "@/lib/userBook";
 
-export default function ShowUserBook({ userBook }: { userBook: UserBook }) {
+export default function ShowUserBook({ userBook }: { userBook: any }) {
   return (
     <section className={styles.info_section}>
       <h1 className={styles.h1}>
@@ -30,7 +29,6 @@ export default function ShowUserBook({ userBook }: { userBook: UserBook }) {
             <li>Local:</li>
             <li>Estado físico:</li>
             <li>Disponibilidade:</li>
-            <li>Status:</li>
           </ul>
           <br />
           <ul className={styles.info_data}>
@@ -40,15 +38,14 @@ export default function ShowUserBook({ userBook }: { userBook: UserBook }) {
             <li>{userBook.book.year}</li>
             <li>{userBook.book.pages} páginas</li>
             <li>{userBook.book.language}</li>
-            <li>{userBook.book.isnb}</li>
+            <li>{userBook.book.isbn}</li>
             <li>{userBook.book.category} </li>
             <li>
               <br />
             </li>
-            <li>{userBook.place} </li>
+            <li>{userBook.user.college} </li>
             <li>{userBook.condition}</li>
-            <li>{getAvailability(userBook.avaliability)}</li>
-            <li>{userBook.status}</li>
+            <li>{getAvailability([userBook.forLoan,userBook.forTrade])}</li>
           </ul>
         </div>
       </div>
